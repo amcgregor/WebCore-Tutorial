f6689b5c4153b39eb92b2d285bfc5e639e9f5512
Alice Bevan–McGregor <alice@gothcandy.com>
2016-04-25 01:15:22 -0400

    Wiring in our stub Article class.

--- wiki/wiki.py
+++ wiki/wiki.py
@@ -1,14 +1,20 @@
 # HTTP status code exception for "302 Not Found" redirection.
 from webob.exc import HTTPFound
 
+from .article import Article
+
 
 class Wiki:
 	"""Basic multi-article editable wiki."""
 	
 	def __init__(self, context=None):
-		pass  # We don't use the context at all.
+		self._ctx = context  # We pass this during Article instantiation.
 	
 	def __call__(self):
 		"""Called to handle direct requests to the web root."""
 		return HTTPFound(location='/Home')  # Redirect
+	
+	def __getattr__(self, name):
+		"""Look up an otherwise unknown child path."""
+		return Article(self._ctx, name)
 
