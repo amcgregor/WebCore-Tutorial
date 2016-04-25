1686e185c609f62395e349d0d84827df0fc1762c
Alice Bevan–McGregor <alice@gothcandy.com>
2016-04-25 01:02:18 -0400

    Basic Wiki site root.

---
+++ wiki/wiki.py
@@ -0,0 +1,14 @@
+# HTTP status code exception for "302 Not Found" redirection.
+from webob.exc import HTTPNotFound
+
+
+class Wiki:
+	"""Basic multi-article editable wiki."""
+	
+	def __init__(self, context=None):
+		pass  # We don't use the context at all.
+	
+	def __call__(self):
+		"""Called to handle direct requests to the web root."""
+		return HTTPFound(location='/Home')  # Redirect
+
